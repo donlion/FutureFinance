c2044cf16448701153afb431bca267c425873ff9
Leo Ørsnes <leo@tattoodo.com>
2017-02-05 09:08:52 +0100

    Transactions limitation

--- src/scripts/components/modules/Transactions.js
+++ src/scripts/components/modules/Transactions.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import Component from '../../Model';
 import Theme from '../../utilities/theme';
 import getPath from 'lodash/get';
+import formatTime from '../../utilities/formatTime';
 // Components
 import Paper from 'material-ui/Paper';
 import Subheader from 'material-ui/Subheader';
@@ -9,6 +10,8 @@ import CircularProgress from 'material-ui/CircularProgress';
 import Transaction from './Transaction';
 import ReactTransitionGroup from 'react-addons-css-transition-group';
 import Avatar from 'material-ui/Avatar';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 // Icons
 import CompareArrows from 'material-ui/svg-icons/action/compare-arrows';
 // Colors
@@ -19,10 +22,31 @@ export default class Transactions extends Component {
         data: PropTypes.object
     };
 
-    get getList() {
-        const {data} = this.props;
+    constructor() {
+        super();
 
-        let list = getPath(data, 'transactions');
+        this.state = {limit: 5};
+    }
+
+    get getTransactions() {
+        const {
+            props: {data},
+            state: {limit}
+        } = this;
+
+        let transactions = getPath(data, 'transactions');
+
+        if (transactions.length > limit) {
+            transactions = transactions.slice(0, limit);
+        }
+
+        return transactions;
+    }
+
+    get getList() {
+        const {getTransactions} = this;
+
+        let list = getTransactions;
 
         let style = {
             display: 'block',
@@ -58,8 +82,8 @@ export default class Transactions extends Component {
                     component="div"
                     className="body"
                     transitionName="animated"
-                    transitionEnterTimeout={500}
-                    transitionLeaveTimeout={300}>
+                    transitionEnterTimeout={600}
+                    transitionLeaveTimeout={400}>
                     {list}
                 </ReactTransitionGroup>
             </div>
@@ -74,10 +98,48 @@ export default class Transactions extends Component {
         return header || false;
     }
 
+    get getTime() {
+        const {data} = this;
+
+        let time = getPath(data, 'time');
+
+        return time || false;
+    }
+
+    get getLimitDropdown() {
+        const {limit} = this.state;
+
+        let onChange = (event, index, value) => this.setState({limit: value});
+
+        return (
+            <DropDownMenu
+                value={limit}
+                onChange={onChange}>
+                <MenuItem
+                    value={1}
+                    primaryText="Limit list to 1 transaction" />
+                <MenuItem
+                    value={5}
+                    primaryText="Limit list to 5 transactions" />
+                <MenuItem
+                    value={10}
+                    primaryText="Limit list to 10 transactions" />
+                <MenuItem
+                    value={20}
+                    primaryText="Limit list to 20 transactions" />
+                <MenuItem
+                    value={50}
+                    primaryText="Limit list to 50 transactions" />
+            </DropDownMenu>
+        );
+    }
+
     render() {
         const {
             getList,
-            getHeader
+            getHeader,
+            getTime,
+            getLimitDropdown
         } = this;
 
         return (
@@ -91,10 +153,13 @@ export default class Transactions extends Component {
                                 marginRight: 12
                             }}
                             color={blue500}
+                            backgroundColor="transparent"
                             icon={<CompareArrows />}
                             size={30} />
-                        {getHeader || Transactions}
+                        <strong>{getHeader || 'Transactions'}</strong>
+                        {getTime ? ` - ${formatTime(getTime)}` : null}
                     </Subheader>
+                    {getLimitDropdown}
                     {getList}
                 </Paper>
             </Theme>
